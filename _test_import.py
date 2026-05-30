36d50db58b30d454d871fa90ff6da1a5abbde36a
root <root@L003.localdomain>
2026-05-30 22:37:23 +0800

    cleanup: remove scaffolding

--- _test_import.py
+++
@@ -1,4 +0,0 @@
-import evolver_tools.vendor.crypto_price as cp
-print('TOOL_META:', cp.TOOL_META)
-print('Callable main:', callable(cp.main))
-print('__name__:', __name__)
--- _verify_meta.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-import sys
-sys.path.insert(0, 'src')
-from evolver_tools.vendor.dice_roll import TOOL_META
-print(TOOL_META)
